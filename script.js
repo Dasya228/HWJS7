354c7ab4612f299bf4d62cef540b534197bb3452
Пользователь <world.worensi@gmail.com>
2023-11-02 14:24:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for найдите сумму элементов этого массива. Запишите ее в переменную result.
+// const handleFunction = (arr)=>{
+//     let res =0
+// for(let i=0;i<arr.length;i++){
+//     res +=arr[i]
+// }
+// return res
+// }
+// console.log(handleFunction([1, 2, 3, 4, 5]))
+
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] => сумму всех элементов массива
+
+// const handleFunction = (arr)=>{
+//     let res = 0
+//     for(let i = 0;i<arr.length;i++){
+//         res += arr[i]
+//     }
+//     return res
+// }
+// console.log(handleFunction([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+
+// Дан массив с элементами [1, 2, 5, 9, 4, 13, 4, 10].
+//С помощью цикла for и оператора if проверьте есть ли в массиве элемент со значением, равным 4.
+// Если есть - выведите на экран 'Есть!' и выйдите из цикла. Если нет - 'Нет!'
+// const handleFunction = (arr)=>{
+//     let res = []
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]===4){
+//            res+='Есть!'
+// break
+//         }else  {
+//              res+='Нет!'
+//         }
+//     }
+//     return res
+//
+// }
+// console.log(handleFunction([1, 2, 5, 9, 4, 13, 4, 10]))
